dd15e0a2821d9cbd5b11814526d068b593a31ad0
Mungo Jackson <Mungo.Jackson@smartstream-stp.com>
2023-06-06 09:22:33 +0100

    Updating Terraform to extract variables

--- basic_wordpress_app/terraform/variables.tf
+++ basic_wordpress_app/terraform/variables.tf
@@ -3,3 +3,22 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc-name" {}
+variable "enable_nat_gateway" {}
+variable "single_nat_gateway" {}
+variable "enable_dns_hostnames" {}
+
+variable "eks_ami_type" {}
+
+variable "eks_node_group_one_name" {}
+variable "eks_node_group_one_instance_type" {}
+variable "eks_node_group_one_min_size" {}
+variable "eks_node_group_one_max_size" {}
+variable "eks_node_group_one_desired_size" {}
+
+variable "eks_node_group_two_name" {}
+variable "eks_node_group_two_instance_type" {}
+variable "eks_node_group_two_min_size" {}
+variable "eks_node_group_two_max_size" {}
+variable "eks_node_group_two_desired_size" {}
